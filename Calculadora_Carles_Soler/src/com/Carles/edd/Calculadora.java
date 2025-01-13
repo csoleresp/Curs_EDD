9bc58ccc405bb9d52ca4d3d81d08d2f842b5d075
csoleresp <csoler.esp@gmail.com>
2025-01-13 12:03:21 +0100

    Add files via upload
    Exercici Calculadora EDD

---
+++ Calculadora_Carles_Soler/src/com/Carles/edd/Calculadora.java
@@ -0,0 +1,26 @@
+package com.Carles.edd;
+
+public class Calculadora {
+
+    public static int suma(int a, int b){
+        return a + b;
+    }
+
+    public static int resta(int a, int b){
+        return a - b;
+    }
+
+    public static int multiplica(int a, int b){
+        return a * b;
+    }
+
+    public static int divideix(int a, int b){
+        try{
+            return a / b;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return 0;
+        }
+    }
+}
---
+++ Calculadora_Carles_Soler/src/com/Carles/edd/Main.java
@@ -0,0 +1,10 @@
+package com.Carles.edd;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculadora.suma(3, 2));
+        System.out.println(Calculadora.resta(5, 4));
+        System.out.println(Calculadora.multiplica(8, 4));
+        System.out.println(Calculadora.divideix(6, 3));
+    }
+}
