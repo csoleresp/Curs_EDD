60a639ed9d44c9eef20c19fdedc2bbe9d63d6019
csoleresp <csoler.esp@gmail.com>
2025-03-04 10:01:12 +0100

    Add files via upload

---
+++ Activitat_maven_NetBeans_Carles_Soler/src/main/java/com/mycompany/activitat_maven_netbeans_carles_soler/Activitat_maven_NetBeans_Carles_Soler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.activitat_maven_netbeans_carles_soler;
+
+/**
+ *
+ * @author Carles
+ */
+public class Activitat_maven_NetBeans_Carles_Soler {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
