63d370e728aec84934ae8f02dd378baabf009803
csoleresp <csoler.esp@gmail.com>
2025-03-04 10:01:49 +0100

    Add files via upload

---
+++ ProjecteGradle/app/src/main/java/com/ieseljust/App.java
@@ -0,0 +1,11 @@
+package com.ieseljust;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hola, mon des de Gradle!");
+    }
+
+    Object getGreeting() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
